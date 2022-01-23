cb44cfd6a22c70ee9fb69fbb6ef78c30b66d1859
Alex Leska <deskmicealex@gmail.com>
2022-01-23 08:14:32 -0800

    first commit

---
+++ docusaurus.config.js
@@ -0,0 +1,117 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/nightOwlLight');
+const darkCodeTheme = require('prism-react-renderer/themes/nightOwl');
+
+const config = {
+  title: 'Harpocrates',
+  tagline: 'Harpocrates was the Greek god of silence, secrets, and confidentiality. He will help you keep your private information safe and secure.',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'leskaa', // Usually your GitHub org/user name.
+  projectName: 'harpocrates', // Usually your repo name.
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Harpocrates',
+        logo: {
+          alt: 'Harpocrates Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Tutorial',
+          },
+          {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/leskaa/',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Hackathon',
+            items: [
+              {
+                label: 'Minnehack',
+                href: 'https://minnehack.io/',
+              },
+              {
+                label: 'Devpost',
+                href: 'https://minnehack-2022.devpost.com/',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discord.com/',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/leskaa',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Harpocrates Team. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
---
+++ src/components/HomepageFeatures.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './HomepageFeatures.module.css';
+
+const FeatureList = [
+  {
+    title: 'Validate Links',
+    Svg: require('../../static/img/undraw_startled.svg').default,
+    description: (
+      <>
+        Harpocrates monitors links sent on your discord server. The bot references these
+        links with Google's Safe Browsing API to inform users of unsafe clicks.
+      </>
+    ),
+  },
+  {
+    title: 'Prevent Personal Info Leaks',
+    Svg: require('../../static/img/undraw_personal_data.svg').default,
+    description: (
+      <>
+        Harpocrates screens new posts from your users. A variety of natural language
+        processing strategies are utilized to prevent users from leaking personal info.
+      </>
+    ),
+  },
+  {
+    title: 'Alert Users About Scams',
+    Svg: require('../../static/img/undraw_alert.svg').default,
+    description: (
+      <>
+        Harpocrates alerts users of new scams on discord. The bot monitors FTC
+        Consumer Information Scam Alerts and can notify your users.
+      </>
+    ),
+  },
+];
+
+function Feature({Svg, title, description}) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} alt={title} />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures() {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
